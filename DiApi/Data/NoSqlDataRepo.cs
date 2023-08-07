1f10eae8d4f2f0a3bee80bd0223413e2f69f1615
Rodrigo Lins <rlinsdev@gmail.com>
2023-08-07 09:39:34 -0300

    Injection

--- DiApi/Data/NoSqlDataRepo.cs
+++ DiApi/Data/NoSqlDataRepo.cs
@@ -1,11 +1,23 @@
+using DiApi.DataServices;
+
 namespace DiApi.Data
 {
     public class NoSqlDataRepo : IDataRepo
     {
+        // Begin - Structure Dependency Injection pattern 
+        private IDataService _dataService;
+
+        public NoSqlDataRepo(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+        // End - Structure Dependency Injection pattern 
+
         public string ReturnData()
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("--> Getting data from Non SQL data base...");
+            _dataService.GetProductData("https://something.api");
             Console.ResetColor();
 
             return "No SQL Data From DB";
