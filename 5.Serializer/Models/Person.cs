f8c2953575fbdf4bb64caaabf98746a90bc84feb
Rodrigo Lins <rlinsdev@gmail.com>
2023-08-10 16:09:23 -0300

    Serialization

---
+++ 5.Serializer/Models/Person.cs
@@ -0,0 +1,11 @@
+namespace Serializer.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set;}
+        public int Age { get; set; }
+        public bool IsAlive { get; set; }
+    }
+}
--- 5.Serializer/Program.cs
+++ 5.Serializer/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using Serializer.Models;
+
+var person = new Person{
+    Id = 1,
+    FirstName = "Sean",
+    LastName = "Connery",
+    Age = 90,
+    IsAlive = false
+};
+
+string jsonString = JsonSerializer.Serialize(person);
+string fileName = "person.json";
+
+File.WriteAllText(fileName, jsonString);
+
+Console.WriteLine(File.ReadAllText(fileName));
