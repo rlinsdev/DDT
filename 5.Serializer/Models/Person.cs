a6ab78192596317097dd4d35896d4d7b9772978b
Rodrigo Lins <rlinsdev@gmail.com>
2023-08-13 20:13:51 -0300

    Renaming properties in JSon

--- 5.Serializer/Models/Person.cs
+++ 5.Serializer/Models/Person.cs
@@ -1,12 +1,17 @@
+using System.Text.Json.Serialization;
+
 namespace Serializer.Models
 {
     public class Person
     {
         public int Id { get; set; }
         public string? FirstName { get; set; }
+
+        [JsonPropertyName("ChangeLatName")]
         public string? LastName { get; set;}
         public int Age { get; set; }
         public bool IsAlive { get; set; }
         public Address? Address {get; set;}
+        public IList<Phone>? Phones{ get; set;}
     }
 }
--- 5.Serializer/Program.cs
+++ 5.Serializer/Program.cs
@@ -11,6 +11,10 @@ var person = new Person{
         StreetName = "main Street",
         City = "New York",
         ZipCode = "234324234"
+    },
+    Phones = new List<Phone>() {
+        new Phone { Type="Home", Number="1654654" },
+        new Phone { Type="Mobile", Number="32323232" }
     }
 };
 
