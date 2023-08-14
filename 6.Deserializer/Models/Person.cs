32aacaa6643e610694a687af5ee06f80330fe7d0
Rodrigo Lins <rlinsdev@gmail.com>
2023-08-14 07:12:27 -0300

    Add 3 classes

---
+++ 6.Deserializer/Models/Person.cs
@@ -0,0 +1,20 @@
+using System.Text.Json.Serialization;
+
+namespace Serializer.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+
+        [JsonPropertyName("ChangeLatName")]
+        public string? LastName { get; set;}
+        public int Age { get; set; }
+
+        [JsonIgnore]
+        public bool IsAlive { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Address? Address {get; set;}
+        public IList<Phone>? Phones{ get; set;}
+    }
+}
---
+++ 6.Deserializer/Models/Phone.cs
@@ -0,0 +1,8 @@
+namespace Serializer.Models
+{
+    public class Phone 
+    {
+        public string? Type { get; set; }
+        public string? Number { get; set; }
+    }
+}
