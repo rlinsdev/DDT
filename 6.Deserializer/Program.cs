62dc891fb21060e16d62ccae07c37ebe33527fa1
Rodrigo Lins <rlinsdev@gmail.com>
2023-08-14 07:51:12 -0300

    Deserialize

--- 6.Deserializer/Program.cs
+++ 6.Deserializer/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using Deserializer.Models;
+
+var opt = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true 
+};
+
+string fileName = "person.json";
+
+string jsonString = File.ReadAllText(fileName);
+
+Person? person = JsonSerializer.Deserialize<Person>(jsonString, opt);
+
+Console.WriteLine($"The first name is: {person!.FirstName}");
