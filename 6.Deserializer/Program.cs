77cc8f62055fc81a4901eaf1e069f645452d39e5
Rodrigo Lins <rlinsdev@gmail.com>
2023-08-15 07:16:02 -0300

    Json Document

--- 6.Deserializer/Program.cs
+++ 6.Deserializer/Program.cs
@@ -1,43 +1,46 @@
-﻿using System.Net.Http.Json;
-using System.Text.Json;
-using Deserializer.Models;
+﻿using System.Text.Json;
 
-var opt = new JsonSerializerOptions
-{
-    PropertyNameCaseInsensitive = true 
-};
+// var opt = new JsonSerializerOptions
+// {
+//     PropertyNameCaseInsensitive = true 
+// };
 
 using HttpClient client = new ()
 {
     BaseAddress = new Uri("http://localhost:5138")
 };
 
-var temperatures = await client.GetFromJsonAsync<Temperature[]>("weatherforecast", opt);
+// var temperatures = await client.GetFromJsonAsync<Temperature[]>("weatherforecast", opt);
 
-if (temperatures != null)
-{
-    foreach (var temp in temperatures)
-    {
-        Console.WriteLine($"Summary: {temp.Summary}");
-    }
-}
-
-// First code
-// var response = await client.GetAsync("weatherforecast");
-
-// if (response.IsSuccessStatusCode) 
+// if (temperatures != null)
 // {
-//     var temperatures = await JsonSerializer.DeserializeAsync<Temperature[]>(await response.Content.ReadAsStreamAsync(), opt);
-
-//     if (temperatures != null)
+//     foreach (var temp in temperatures)
 //     {
-    // foreach (var temp in temperatures)
-    //     {
-    //         Console.WriteLine($"Summary: {temp.Summary}");
-    //     }
+//         Console.WriteLine($"Summary: {temp.Summary}");
 //     }
 // }
-// else
-// {
-//     Console.WriteLine($"Whoops! Error {response.StatusCode}");
-// }
+
+var response = await client.GetAsync("weatherforecast");
+
+if (response.IsSuccessStatusCode) 
+{
+    var jsonString = await response.Content.ReadAsStringAsync();
+
+    using (JsonDocument jsonDocument = JsonDocument.Parse(jsonString))
+    {
+        JsonElement root = jsonDocument.RootElement;
+
+        Console.WriteLine(root.ValueKind);
+
+        foreach (var item in root.EnumerateArray())
+        {
+            Console.WriteLine(item.GetProperty("summary").ToString());
+
+        }
+
+    }
+}
+else
+{
+    Console.WriteLine($"Whoops! Error {response.StatusCode}");
+}
