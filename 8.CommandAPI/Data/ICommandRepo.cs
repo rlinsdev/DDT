3ae61c1293aed3ddea4e11da94973aad7e0b1802
Rodrigo Lins <rlinsdev@gmail.com>
2023-08-17 08:01:39 -0300

    Repository Interface

---
+++ 8.CommandAPI/Data/ICommandRepo.cs
@@ -0,0 +1,12 @@
+namespace CommandAPI.Data
+{
+    public interface ICommandRepo
+    {
+        Task SaveChangesAsync();
+        Task<Command> GetCommandByIdAsync(string commandId);
+        Task<IEnumerable<Command>> GetAllCommandsAsync();
+        Task CreateCommandAsync(Command cmd);
+        //Update?
+        void DeleteCommand(Command cmd);
+    }
+}
