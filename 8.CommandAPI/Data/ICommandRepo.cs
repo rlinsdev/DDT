da26ab12ddaf9d380331161d7078733b9e5c13f6
Rodrigo Lins <rlinsdev@gmail.com>
2023-08-17 08:28:42 -0300

    implementation of interface - repository

--- 8.CommandAPI/Data/ICommandRepo.cs
+++ 8.CommandAPI/Data/ICommandRepo.cs
@@ -1,9 +1,11 @@
+using CommandAPI.Models;
+
 namespace CommandAPI.Data
 {
     public interface ICommandRepo
     {
         Task SaveChangesAsync();
-        Task<Command> GetCommandByIdAsync(string commandId);
+        Task<Command?> GetCommandByIdAsync(string commandId);
         Task<IEnumerable<Command>> GetAllCommandsAsync();
         Task CreateCommandAsync(Command cmd);
         //Update?
