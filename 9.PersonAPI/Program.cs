b2e799114ef6264d2c7f98e95fb7323e7c6274ee
Rodrigo Lins <rlinsdev@gmail.com>
2023-08-20 17:16:31 -0300

    Add sqlserver use

--- 9.PersonAPI/Program.cs
+++ 9.PersonAPI/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PersonAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlDbConnection")));
 
 var app = builder.Build();
 
