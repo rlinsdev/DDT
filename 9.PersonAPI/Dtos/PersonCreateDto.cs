a322d7e3e0bb78db511eb27670d3e424e8a7ddbf
Rodrigo Lins <rlinsdev@gmail.com>
2023-08-23 07:04:22 -0300

    DTO to create a person

---
+++ 9.PersonAPI/Dtos/PersonCreateDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonAPI.Dtos
+{
+    public class PersonCreateDto
+    {
+        [Required]
+        public string? FullName { get; set; }
+
+        [Required]
+        public string? Telephone { get; set; }
+        
+        [Required]
+        public string? DoB {get; set;}
+    }
+}
