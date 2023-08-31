79b89fade22cbdf260048434ec96750b585a267c
Rodrigo Lins <rlinsdev@gmail.com>
2023-08-31 10:40:02 -0300

    Add Db Context

--- 10.ToDoAPI/Program.cs
+++ 10.ToDoAPI/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("ToDoDB"));
 
 var app = builder.Build();
 
