a853b99651af76181cbdb4c719bcb04d040aa40f
Rodrigo Lins <rlinsdev@gmail.com>
2023-08-31 08:13:03 -0300

    Add Get and Post method

--- 10.ToDoAPI/Program.cs
+++ 10.ToDoAPI/Program.cs
@@ -1,11 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Data;
+using ToDoAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("ToDoDB"));
 
 var app = builder.Build();
+
+app.MapGet("api/todo", async (AppDbContext context) => {
+    var items = await context.ToDos.ToListAsync();
+
+    return Results.Ok(items);
+});
+
+app.MapPost("api/todo", async (AppDbContext context, ToDo toDo) => {
+
+    // TODO: Validate object here!
+    await context.ToDos.AddAsync(toDo);
+    await context.SaveChangesAsync();
+    return Results.Created($"api/todo/{toDo.Id}", toDo);
+});
 
 app.Run();
 
