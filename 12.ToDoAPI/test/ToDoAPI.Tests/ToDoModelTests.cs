c1bfaedbd0569924d287997fb6b1db386312f579
Rodrigo Lins <rlinsdev@gmail.com>
2023-09-09 07:32:23 -0300

    UnityTest Structure

---
+++ 12.ToDoAPI/test/ToDoAPI.Tests/ToDoModelTests.cs
@@ -0,0 +1,18 @@
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Tests
+{
+    public class ToDoModelTests
+    {
+        [Fact]
+        public void CanChangeName()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+    }
+}
