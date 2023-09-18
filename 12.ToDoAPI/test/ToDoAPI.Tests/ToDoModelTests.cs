02232c7d257578de271f2054fc93742590bd8888
Rodrigo Lins <rlinsdev@gmail.com>
2023-09-18 08:21:04 -0300

    add unity test

--- 12.ToDoAPI/test/ToDoAPI.Tests/ToDoModelTests.cs
+++ 12.ToDoAPI/test/ToDoAPI.Tests/ToDoModelTests.cs
@@ -8,11 +8,18 @@ namespace ToDoAPI.Tests
         public void CanChangeName()
         {
             // Arrange
+            var testToDo = new ToDoItem
+            {
+                Name = "Complete Unit Test",
+                fee = 10.33,
+                tax = 20
+            };
 
             // Act
+            testToDo.Name = "Complete Azure PipeLine";
 
             // Assert
+            Assert.Equal("Complete Azure PipeLine", testToDo.Name);
         }
-
     }
 }
